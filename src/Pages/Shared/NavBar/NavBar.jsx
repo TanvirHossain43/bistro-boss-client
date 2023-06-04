7469c759ffe67b00f81743e1da3945b14ad29279
unknown <TanvirHossain43>
2023-06-04 14:35:17 +0600

    food cart added

--- src/Pages/Shared/NavBar/NavBar.jsx
+++ src/Pages/Shared/NavBar/NavBar.jsx
@@ -1,11 +1,14 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../../Providers/AuthProvider";
+import { FaShoppingCart } from 'react-icons/fa';
+import useCart from "../../../hooks/useCart";
 
 
 
 const NavBar = () => {
     const { user, logOut } = useContext(AuthContext)
+    const [cart] = useCart()
 
     const handleLogOut = () => {
         logOut()
@@ -20,6 +23,12 @@ const NavBar = () => {
         <li><Link to="/menu">Our menu</Link></li>
         <li><Link to="/order/salad">Order Food</Link></li>
         <li><Link to="/Secret">Secret</Link></li>
+        <li><Link to="/">
+            <button className="btn">
+                <FaShoppingCart></FaShoppingCart>
+                <div className="badge badge-secondary">+{cart?.length || 0}</div>
+            </button>
+        </Link></li>
 
         {
             user ? <>
